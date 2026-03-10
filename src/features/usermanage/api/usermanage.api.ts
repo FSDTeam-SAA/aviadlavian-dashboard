e01fe0656401e4e3f3caaeec3b70539982b1bf91
iftykhar <s.m.ifty49@gmail.com>
2026-03-10 23:21:30 +0600

    fix: usermanagement

--- src/features/usermanage/api/usermanage.api.ts
+++ src/features/usermanage/api/usermanage.api.ts
@@ -4,8 +4,14 @@
 
 import { api } from "@/lib/api";
 import { GetAllUsersResponse } from "../types/usermanage.types";
+import { getSession } from "next-auth/react";
 
 export async function getAllUsers(): Promise<GetAllUsersResponse> {
-  const res = await api.get<GetAllUsersResponse>("/user/get-all-users");
+  const session = await getSession();
+  const res = await api.get<GetAllUsersResponse>("/user/get-all-users", {
+    headers: {
+      Authorization: `Bearer ${session?.accessToken}`,
+    },
+  });
   return res.data;
 }
